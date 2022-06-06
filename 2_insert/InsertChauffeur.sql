812142a4fa6acfe3f3273794e2f6ebe00a7f34cb
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2022-06-06 16:16:54 +0300

    insert into table chauffeur

---
+++ 2_insert/InsertChauffeur.sql
@@ -0,0 +1,11 @@
+INSERT INTO chauffeur(nom,prenom,cin,contact,email) VALUES('RAKOTONDRABE','Mahazaka','102071026132','+261334657920','mahazaka@gmail.com'),
+    ('RAKOTOMANDIMBY','Zazaharivelo','102036047142','+261320146589','zazaharivelo@gmail.com'),
+    ('RABEARIVELO','Zakarison','102476413210','+261384695710','zakarison@gmail.com'),
+    ('RABETOKOTANY','Lucien','102356142259','+261324567139','lucien@gmail.com'),
+    ('RANARIVELO','Louis','102473103532','+261334231536','louis01@gmail.com'),
+    ('RAVELO','Jean-Pierre','102153541204','+261381034560','jeanpierre80@gmail.com'),
+    ('RAMALINGOM','Alexandre','102223031364','+261342606120','alexandre031@gmail.com'),
+    ('ANDRIAMANORO','Dieu-Donné','102325531234','+261341234560','dieudonne@gmail.com'),
+    ('RABE','Niaina','102860210247','+261332900320','niaina@gmail.com'),
+    ('RANDRIA','Marc','102256531234','+261321234521','marc@gmail.com'),
+;
