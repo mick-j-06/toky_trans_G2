38b69e18ca8e8f1ae80692f5a12274414885fa9d
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2022-06-14 13:08:01 +0300

    Modify inserts and requests

--- 2_insert/InsertVoyage.sql
+++ 2_insert/InsertVoyage.sql
@@ -9,4 +9,11 @@ INSERT INTO voyage(id_ville_depart,id_ville_arrivee,id_offre,date_voyage,heure_d
 
 --depart antananarivo -> mahajanga
 --offre province_simple
-    (1,6,3,'16-06-22','06:00:00',0,3);
+    (1,6,3,'16-06-22','06:00:00',0,3),
+--depart mahajanga -> antsiranana
+--offre province premium
+    (6,4,4,'18-06-22','07:00:00',4,3),
+--depart fianarantsoa -> antsiranana
+--offre province simple
+    (5,4,3,'17-06-22','18:00:00',2,9)
+;
